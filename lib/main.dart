b16b2f306707b586d75da011799677cc9d4b7a4e
Pratham Sarankar <pratham@sarankar.com>
2025-09-27 00:59:12 +0530

    Android Setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:survey_app/models/survey_entry.dart';
 import 'config/service_locator.dart';
 import 'providers/auth_provider.dart';
 import 'providers/survey_provider.dart';
@@ -11,10 +12,10 @@ import 'config/app_config.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+
   // Setup service locator
   await setupServiceLocator();
-  
+
   runApp(const MyApp());
 }
 
@@ -49,7 +50,7 @@ class MyApp extends StatelessWidget {
             border: OutlineInputBorder(),
             contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
           ),
-          cardTheme: CardTheme(
+          cardTheme: CardThemeData(
             elevation: 2,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(12),
@@ -65,7 +66,7 @@ class MyApp extends StatelessWidget {
         },
         onGenerateRoute: (settings) {
           if (settings.name == '/entry-details') {
-            final entry = settings.arguments;
+            final entry = settings.arguments as SurveyEntry?;
             return MaterialPageRoute(
               builder: (context) => AddEntryScreen(entry: entry),
             );
